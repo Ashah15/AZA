94142944071d898e2e5d7fee8c23e87bcbcd2f5e
Ashah15 <ashaabdillahi53@gmail.com>
2022-03-11 12:21:23 +0300

    Remove unnecessary spaces to file

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,27 +1,21 @@
 class TransactionController < ApplicationController
   before_action :logged_in_user
 
-
 	def create
       @transaction = Transaction.new(transactions_params)
       #@transaction.customer = current_user
       #redirect_to transactions_path
       if @transaction.save!
       render json: @transaction.to_json
-      end
-
-
-		
+      end	
 	end
 
 	def index
-	  @transaction = Transaction.all	
-		
+	  @transaction = Transaction.all		
 	end
 
 	def show
       @transaction = Transaction.find(params[:id])
-		
 	end
 
 	private
@@ -33,6 +27,5 @@ class TransactionController < ApplicationController
 	def transaction_owner
 	  @transaction = Transaction.find(params[:id])
 	  redirect_to transactions_path unless current_user == current_customer
-		
 	end
 end
